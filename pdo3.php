6c4ed666681ee00a615d9b4821f9d425680d6444
Sabbir Ahmed <sabbirbd912@gmail.com>
2025-11-03 14:58:09 +0600

    PDO small Project

--- pdo3.php
+++ pdo3.php
@@ -2,19 +2,20 @@
 // Data Read From Database
 
 $dsn = "sqlite:data.db";
-$sql="SELECT * FROM users";
+$sql="SELECT * FROM users WHERE id < ?";
+$sql="SELECT * FROM users WHERE id BETWEEN ? AND ?";
 
 try {
     $pdo=new PDO($dsn);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $result =$pdo->query($sql);
-    //1.1 All data get by while-loop
+    $statement= $pdo->prepare($sql);
+    $statement->execute([2,5]);
+
     // while($row=$result->fetch(PDO::FETCH_ASSOC)){
     //     print_r($row);
     // }
 
-    //1.2 All data fetch
-    $all=$result->fetchAll(PDO::FETCH_ASSOC);
+    $all=$statement->fetchAll(PDO::FETCH_ASSOC);
     print_r($all);
 
    // -----------------------------------------34 restrat
---
+++ pdoProject.php
@@ -0,0 +1,28 @@
+<?php
+$dsn="sqlite:country.db";
+
+try {
+    $pdo=new PDO ($dsn);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->exec(
+        "CREATE TABLE IF NOT EXISTS countries(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name VARCHAR(50) UNIQUE,
+        capital VARCHAR(50),
+        population INTEGER
+        )"
+    );
+
+    $statement=$pdo->prepare("INSERT INTO countries(name,capital,population) VALUES(?,?,?)");
+    $file = fopen("data.csv","r");
+    $row =fgetcsv($file);
+    $pdo->beginTransaction();
+    while(($row = fgetcsv($file))!==false){
+        $statement->execute([$row[0],$row[1], $row[2]]);
+    }
+    $pdo->commit();
+    // print_r($row);
+} catch (PDOException $e) {
+    $pdo->rollBack();
+    echo $e->getMessage();
+}
