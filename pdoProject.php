28d8d8aa8fe3f14ab781d95748ac9851d400f9c5
Sabbir Ahmed <sabbirbd912@gmail.com>
2025-11-03 15:15:26 +0600

    PDO Clean by set delete Query

--- pdoProject.php
+++ pdoProject.php
@@ -1,8 +1,8 @@
 <?php
-$dsn="sqlite:country.db";
+$dsn = "sqlite:country.db";
 
 try {
-    $pdo=new PDO ($dsn);
+    $pdo = new PDO($dsn);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->exec(
         "CREATE TABLE IF NOT EXISTS countries(
@@ -13,12 +13,14 @@ try {
         )"
     );
 
-    $statement=$pdo->prepare("INSERT INTO countries(name,capital,population) VALUES(?,?,?)");
-    $file = fopen("data.csv","r");
-    $row =fgetcsv($file);
+    $pdo->exec('DELETE FROM countries');
+
+    $statement = $pdo->prepare("INSERT INTO countries(name,capital,population) VALUES(?,?,?)");
+    $file = fopen("data.csv", "r");
+    $row = fgetcsv($file);
     $pdo->beginTransaction();
-    while(($row = fgetcsv($file))!==false){
-        $statement->execute([$row[0],$row[1], $row[2]]);
+    while (($row = fgetcsv($file)) !== false) {
+        $statement->execute([$row[0], $row[1], $row[2]]);
     }
     $pdo->commit();
     // print_r($row);
